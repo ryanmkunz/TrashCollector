a5b8e468d4c519f7b11951e0537c9d57365f34fc
Ryan Kunz <ryanmkunz@gmail.com>
2019-04-30 15:56:43 -0500

    Trying to add roles, not working
    same website referenced in last commit, I have errors with (m => m.UserName etc. because there is no definition for UserName

---
+++ TrashCollection/TrashCollection/Controllers/RoleController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TrashCollection.Models;
+
+namespace TrashCollection.Controllers
+{
+    public class RoleController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Role
+        public ActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!isAdminUser())
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
